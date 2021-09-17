9bd8b19c570a9ef352926cf9c674d927bc5a5d72
hamza bouthour <61101578+hamza-bouthour@users.noreply.github.com>
2021-09-17 08:36:03 -0700

    database connection established

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const env = process.env;
 
 
 // local mysql db connection
-
+const remoteDB = 'mysql://b4bca88606d8fb:452f3c94@us-cdbr-east-04.cleardb.com/heroku_3240e6af6cc9cb8?reconnect=true';
 const config = {
     db: {
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'gigit',
+        host: 'us-cdbr-east-04.cleardb.com',
+        user: 'b4bca88606d8fb',
+        password: '452f3c94',
+        database: 'heroku_3240e6af6cc9cb8',
         waitForConnections: true
     }
 }
